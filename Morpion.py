8b9cf441bce13715d5767fefed2a6ceaac583fc6
Genesis <d.ananda974@gmail.com>
2017-12-07 09:06:00 +0100

    init commit

---
+++ Morpion.py
@@ -0,0 +1,28 @@
+def joueur1 (choix):
+    choix=input(string)
+    if choix == "a1":
+        grille [0,0]="X"
+
+    if choix == "a2":
+        grille[0,1]="X"
+
+    if choix == "a3":
+        grille[0,2]="X"
+
+    if choix == "b1":
+        grille[1,0]="X"
+
+    if choix == "b2":
+        grille[1,1]="X"
+
+    if choix == "b3":
+        grille[1,2]="X"
+
+    if choix == "c1":
+        grille[2,0]="X"
+
+    if choix == "c2":
+        grille[2,1]="X"
+
+    if choix == "c3":
+        grille[2,2]="X"
